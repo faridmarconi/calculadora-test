58196f2ec4c268e3a7d2255c87ac39adc9f2886d
faridmarconi <faridmarconi@gmail.com>
2023-04-25 16:21:35 -0300

    calculadora base criada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Calculator from "./components/Calculator";
+
 function App() {
   return (
-    <div className="App">
-      Calculadora App
+    <div>
+      <Calculator />
     </div>
   );
 }
